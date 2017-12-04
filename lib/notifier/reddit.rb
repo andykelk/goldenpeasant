4ebab76d0b97f8e907c12e21687237f17869d7a7
Andy Kelk <andy@marketplacer.com>
2017-12-05 07:08:04 +1100

    Fix up variable scoping

--- lib/notifier/reddit.rb
+++ lib/notifier/reddit.rb
@@ -4,7 +4,7 @@ module Notifier
   class Reddit
     include Logging
     def initialize(credentials) 
-      reddit = Redd.it(
+      @reddit = Redd.it(
         user_agent: 'Redd:GoldenPheasant:v1.0.0 (by /u/mopoke)',
         client_id:  credentials[:client_id],
         secret:     credentials[:secret],
@@ -16,7 +16,7 @@ module Notifier
 
     def notify title, url
       begin
-        reddit.subreddit('gimlet').submit(title, url: url, sendreplies: false)
+        @reddit.subreddit('gimlet').submit(title, url: url, sendreplies: false)
       rescue Redd::APIError => e
         raise unless e.message =~ /already been submitted/
       end
