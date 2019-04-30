19d40b0b83e6ead9fab0f36414996f756afc0548
Andy Kelk <andy@andykelk.net>
2019-04-30 04:56:23 +0000

    Update for new GimletMedia site
    Changed URLs, changed page layout.

--- lib/parser/gimlet_media.rb
+++ lib/parser/gimlet_media.rb
@@ -7,11 +7,11 @@ module Parser
       feed, html = opts.values_at(:feed, :html)
       items = []
       feed_title = html.at_css("meta[@property='og:title']")['content']
-      feed_title.gsub!(/ - All Episodes$/i, '')
+      feed_title.gsub!(/^Episodes \| /i, '')
 
-      html.css('div.episode').each do |item|
+      html.css('div.episode-card').each do |item|
         url = URI.join(feed.url, item.at_css('a')['href']).to_s
-        title = item.at_css('h2.episode-title').content
+        title = item.at_css('p.episode-title').content.strip
         items << Item.new(url: url, title: "#{feed_title} - #{title}", feed: feed)
       end
       items
